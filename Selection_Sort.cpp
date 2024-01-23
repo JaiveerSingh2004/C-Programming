8ab8ff59b4d6033620dd11b87fe0a97afc4bf761
Jaiveer Singh <jaiveer2104.be23@chitkara.edu.in>
2024-01-23 19:07:50 +0530

    After Practice

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -3,6 +3,7 @@ using namespace std;
  
 int main()
 {
+    /*
     int arr[5] = {5,8,7,9,2};
 
     for(int i = 0; i<=4; i++)
@@ -21,5 +22,49 @@ int main()
     {
         cout<<arr[i]<<" ";
     }
+    */
+
+    // /*
+    // Selection Sort in Array
+    int n;
+    cout<<"Enter the size of array: ";cin>>n;
+
+    int arr[n];
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<"Enter num at index"<<i<<" :";
+        cin>>arr[i];
+    }
+
+    cout<<"Without Sorting: "<<endl;
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+cout<<endl;
+    for(int i = 0; i<=n-1; i++)
+    {
+        int index = i;
+        for(int j = i+1; j<n; j++)
+        {
+            if (arr[j] < arr[index])
+            {
+                index = j;
+            }
+        }
+        swap(arr[index], arr[i]);
+    }
+
+    cout<<"After Sorting: "<<endl;
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    
+    // */
  return 0;
 }
